c07211b314ea2415aa2a7fd3e2f78f6dd9abf36b
Igor Storchevoy <hellsgor@gmail.com>
2025-09-25 17:33:53 +0400

    Feat[main/config]: added chapters list

---
+++ src/pages/main/config/chapters-list.ts
@@ -0,0 +1,9 @@
+import { ROUTES } from '@/shared/config';
+
+export const CHAPTERS_LIST = [
+  {
+    href: ROUTES.gsapTo,
+    text: 'GSAP To',
+    description: 'lorem ipsum dolor',
+  },
+] as const;
