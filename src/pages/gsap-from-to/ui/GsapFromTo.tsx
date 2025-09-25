54c2b4d73deaae438f0156cc12040c7cb92fd85f
Igor Storchevoy <hellsgor@gmail.com>
2025-09-26 00:32:51 +0400

    Feat[pages]: created FromTo page

---
+++ src/pages/gsap-from-to/ui/GsapFromTo.tsx
@@ -0,0 +1,13 @@
+import { Container } from '@/shared/ui';
+
+export function GsapFromTo() {
+  return (
+    <>
+      <section>
+        <Container>
+          <p>fromTo</p>
+        </Container>
+      </section>
+    </>
+  );
+}
