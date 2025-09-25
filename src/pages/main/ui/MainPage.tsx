a83a47dd2a1c0e706ac6a61dedcc07b4016e91d0
Igor Storchevoy <hellsgor@gmail.com>
2025-09-25 17:34:08 +0400

    Update[main/ui]: integrate chapters list into MainPage component

--- src/pages/main/ui/MainPage.tsx
+++ src/pages/main/ui/MainPage.tsx
@@ -1,10 +1,21 @@
-import { Container } from '@/shared/ui';
+import { Chapter, Container } from '@/shared/ui';
+import st from './MainPage.module.scss';
+import { CHAPTERS_LIST } from '../config/chapters-list';
 
 export function MainPage() {
   return (
     <section>
       <Container>
         <h1>GSAP Animations</h1>
+        <ol className={st.chaptersList}>
+          {CHAPTERS_LIST.map((chapter) => (
+            <Chapter
+              href={chapter.href}
+              text={chapter.text}
+              description={chapter.description}
+            />
+          ))}
+        </ol>
       </Container>
     </section>
   );
