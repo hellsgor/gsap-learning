22108418d0d8c9eeef0a6c05efcd018d20369504
Igor Storchevoy <hellsgor@gmail.com>
2025-09-26 00:36:54 +0400

    Update[pages/gsap-from-to]: added content

--- src/pages/gsap-from-to/ui/GsapFromTo.tsx
+++ src/pages/gsap-from-to/ui/GsapFromTo.tsx
@@ -5,7 +5,32 @@ export function GsapFromTo() {
     <>
       <section>
         <Container>
-          <p>fromTo</p>
+          <div className="content">
+            <h1>GsapFromTo</h1>
+            <p>
+              Метод <code>gsap.fromTo()</code> используется для анимации
+              элементов из нового состояния в новое состояние.
+            </p>
+            <p>
+              Метод <code>gsap.fromTo()</code> похож на методы{' '}
+              <code>gsap.from()</code> и <code>gsap.to()</code>, но разница в
+              том, что <code>gsap.fromTo()</code> анимирует элементы из нового
+              состояния в новое состояние, в то время как{' '}
+              <code>gsap.from()</code> анимирует элементы из нового состояния к
+              их текущему состоянию, а <code>gsap.to()</code> — из их текущего
+              состояния в новое состояние.
+            </p>
+            Узнайте больше о методе{' '}
+            <code>
+              <a
+                href="https://gsap.com/docs/v3/GSAP/gsap.fromTo()"
+                target="_blank"
+              >
+                gsap.fromTo()
+              </a>
+            </code>
+            .
+          </div>
         </Container>
       </section>
     </>
