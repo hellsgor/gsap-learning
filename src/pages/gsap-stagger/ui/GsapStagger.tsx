bf79cd9c847200d0f45e870e0c65b0b27e5802bb
Igor Storchevoy <hellsgor@gmail.com>
2025-09-26 23:30:26 +0400

    Update[pages/gsap-stagger]: added content

--- src/pages/gsap-stagger/ui/GsapStagger.tsx
+++ src/pages/gsap-stagger/ui/GsapStagger.tsx
@@ -1,3 +1,37 @@
+import { Container } from '@/shared/ui';
+
 export function GsapStagger() {
-  return <div>GsapStagger</div>;
+  return (
+    <>
+      <section>
+        <Container>
+          <div className="content">
+            <h1>GsapStagger</h1>
+            <p>
+              GSAP stagger — это функция, которая позволяет применять анимации с
+              пошаговой задержкой к группе элементов.
+            </p>
+            <p>
+              Используя функцию stagger в GSAP, можно задать время задержки
+              между анимациями каждого элемента, а также настроить сглаживание
+              (easing) и длительность каждой отдельной анимации. Это позволяет
+              создавать динамичные и визуально привлекательные эффекты, такие
+              как последовательное появление (staggered fades), вращения,
+              движения и многое другое.
+            </p>
+            <p>
+              Узнайте больше о функции{' '}
+              <a
+                href="https://gsap.com/resources/getting-started/Staggers"
+                target="_blank"
+              >
+                Gsap Stagger
+              </a>
+              .
+            </p>
+          </div>
+        </Container>
+      </section>
+    </>
+  );
 }
