e534ca729df138d149f4a8470677cf4b03a378db
Igor Storchevoy <hellsgor@gmail.com>
2025-09-29 22:41:17 +0400

    Update[pages/gsap-scroll-trigger]: added content

--- src/pages/gsap-scroll-trigger/ui/GsapScrollTrigger.tsx
+++ src/pages/gsap-scroll-trigger/ui/GsapScrollTrigger.tsx
@@ -1,3 +1,35 @@
+import { Container } from '@/shared/ui';
+
 export function GsapScrollTrigger() {
-  return <div>GsapScrollTrigger</div>;
+  return (
+    <>
+      <section>
+        <Container>
+          <div className="content">
+            <h1>Gsap ScrollTrigger</h1>
+            <p>
+              Gsap ScrollTrigger — это плагин, который позволяет создавать
+              анимации, запускаемые позицией прокрутки страницы.
+            </p>
+            <p>
+              С помощью ScrollTrigger можно задавать различные действия, которые
+              будут запускаться в определённых точках скролла, например: запуск
+              или завершение анимации, «прокрутка» анимаций вместе с
+              пользователем, закрепление элементов на экране и многое другое.
+            </p>
+            <p>
+              Узнайте больше о методе{' '}
+              <a
+                href="https://gsap.com/docs/v3/Plugins/ScrollTrigger/"
+                target="_blank"
+              >
+                gsap scroll trigger
+              </a>
+              .
+            </p>
+          </div>
+        </Container>
+      </section>
+    </>
+  );
 }
